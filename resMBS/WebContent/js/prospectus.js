9abc38cf014e6bbd8faecb0614a57bd82f3e6a37
Supun <supunsnnk@gmail.com>
2019-01-16 16:35:20 +0530

    Update prospectus.js

--- resMBS/WebContent/js/prospectus.js
+++ resMBS/WebContent/js/prospectus.js
@@ -139,11 +139,160 @@ function getDataForGivenYear(year, id_chart, id_specific_topic, repet) {
 				}
 			}
 		});
+		var topic = [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+			0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ];
 
+	var new_arr = [];
+	data.forEach(function(d) {
+		//console.log(d);
+		if (d[1] == year) {
+			if (d[4] == 'w1') {
+				topic[0] = topic[0] + parseFloat(d[3]);
+				//console.log('d[1]=',d[1],'d[4]=',d[4],'d[3]=',d[3],'	topic[0]=',	topic[0]);
+			} else if (d[4] == 'w2') {
+				topic[1] = topic[1] + parseFloat(d[3]);
+			} else if (d[4] == 'w3') {
+				topic[2] = topic[2] + parseFloat(d[3]);
+			} else if (d[4] == 'w4') {
+				topic[3] = topic[3] + parseFloat(d[3]);
+			} else if (d[4] == 'w5') {
+				topic[4] = topic[4] + parseFloat(d[3]);
+			}
+
+			else if (d[4] == 'w6') {
+				topic[5] = topic[5] + parseFloat(d[3]);
+			} else if (d[4] == 'w7') {
+				topic[6] = topic[6] + parseFloat(d[3]);
+			} else if (d[4] == 'w8') {
+				topic[7] = topic[7] + parseFloat(d[3]);
+			} else if (d[4] == 'w9') {
+				topic[8] = topic[8] + parseFloat(d[3]);
+			} else if (d[4] == 'w10') {
+				topic[9] = topic[9] + parseFloat(d[3]);
+			} else if (d[4] == 'w11') {
+				topic[10] = topic[10] + parseFloat(d[3]);
+			} else if (d[4] == 'w12') {
+				topic[11] = topic[11] + parseFloat(d[3]);
+			} else if (d[4] == 'w13') {
+				topic[12] = topic[12] + parseFloat(d[3]);
+			} else if (d[4] == 'w14') {
+				topic[13] = topic[13] + parseFloat(d[3]);
+			} else if (d[4] == 'w15') {
+				topic[14] = topic[14] + parseFloat(d[3]);
+			} else if (d[4] == 'w16') {
+				topic[15] = topic[15] + parseFloat(d[3]);
+			} else if (d[4] == 'w17') {
+				topic[16] = topic[16] + parseFloat(d[3]);
+			} else if (d[4] == 'w18') {
+				topic[17] = topic[17] + parseFloat(d[3]);
+			} else if (d[4] == 'w19') {
+				topic[18] = topic[18] + parseFloat(d[3]);
+			} else if (d[4] == 'w20') {
+				topic[19] = topic[19] + parseFloat(d[3]);
+			} else if (d[4] == 'w21') {
+				topic[20] = topic[20] + parseFloat(d[3]);
+			} else if (d[4] == 'w22') {
+				topic[21] = topic[21] + parseFloat(d[3]);
+			} else if (d[4] == 'w23') {
+				topic[22] = topic[22] + parseFloat(d[3]);
+			} else if (d[4] == 'w24') {
+				topic[23] = topic[23] + parseFloat(d[3]);
+			} else if (d[4] == 'w25') {
+				topic[24] = topic[24] + parseFloat(d[3]);
+			} else if (d[4] == 'w26') {
+				topic[25] = topic[25] + parseFloat(d[3]);
+			} else if (d[4] == 'w27') {
+				topic[26] = topic[26] + parseFloat(d[3]);
+			} else if (d[4] == 'w28') {
+				topic[27] = topic[27] + parseFloat(d[3]);
+			} else if (d[4] == 'w29') {
+				topic[28] = topic[28] + parseFloat(d[3]);
+			} else if (d[4] == 'w30') {
+				topic[29] = topic[29] + parseFloat(d[3]);
+			}
+		}
+
+	});
+	var temp_year='Prospectus_of_'+year;
+	var chart = c3.generate({
+		data : {
+			columns : [
+					[ temp_year, topic[0], topic[1], topic[2], topic[3],
+							topic[4], topic[5], topic[6], topic[7],
+							topic[8], topic[9], topic[10], topic[11],
+							topic[12], topic[13], topic[14], topic[15],
+							topic[16], topic[17], topic[18], topic[19],
+							topic[20], topic[21], topic[22], topic[23],
+							topic[24], topic[25], topic[26], topic[27],
+							topic[28], topic[29] ],
+
+			],
+			type : 'bar',
+			groups : [ [ temp_year ] ],
+			onclick : function(d, element) { 
+				showPidWhenGivenTopicAndYear(d, data,id_specific_topic,repet);
+			}
+		},
+		bindto : '#'+id_chart + repet,
+		grid : {
+			y : {
+				lines : [ {
+					value : 0
+				} ]
+			}
+		},
+		axis : {
+			x : {
+				label : {
+					text : 'topics',
+					position : 'outer-middle',
+				},
+				type : 'category',
+				tick : {
+					rotate : 85,
+					multiline : false
+				},
+				categories : [ 'w1', 'w2', 'w3', 'w4', 'w5', 'w6',
+						'w7', 'w8', 'w9', 'w10', 'w11', 'w12', 'w13',
+						'w14', 'w15', 'w16', 'w17', 'w18', 'w19',
+						'w20', 'w21', 'w22', 'w23', 'w24', 'w25',
+						'w26', 'w27', 'w28', 'w29', 'w30' ]
+			},
+			tick : {
+				x : {
+
+					multiline : false,
+					culling : {
+						max : 1
+					},
+				},
+				label : {
+					text : 'topics',
+					position : 'center-bottom',
+
+				},
+			},
+			y : {
+				label : {
+					text : ' Sum of Nominal values (Million $)',
+					position : 'outer-middle',
+				},
+//				max : 33,
+//				min : 0,
+//				padding : {
+//					top : 0,
+//					bottom : 0
+//				}
+			}
+		}
+	});
+
+});
 		// -----
 
 		// --- second graph
-		var topic = [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		//-----------------------------------------------------------------------------
+		/*var topic = [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 				0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ];
 
 		var new_arr = [];
@@ -305,13 +454,97 @@ function getDataForGivenYear(year, id_chart, id_specific_topic, repet) {
 			}
 		});
 
-	});
+	});*/
 }
 // drawing third graph when necessary use this function
 function showPidWhenGivenTopicAndYear(event_data, data, id_specific_topic,repet) {
-	console.log("id_specific_topic", id_specific_topic);
+	console.log("event_data",event_data);
 	var idString=event_data.id+repet;
-	document.getElementById(idString).innerHTML = 'Associated topic  T'
+	document.getElementById(idString).innerHTML = 'Associated topic  w'+(event_data.index+1);
+	var PidArray = [];
+	var WeightAray = [ 'Weight-PID' ];
+	var countPid = 0;
+	console.log("data",data[0]);
+	//console.log("event_data",event_data);
+	var only_year_str= event_data.id.split("_");
+	//console.log('only_year_str',only_year_str[2]);
+	data.forEach(function(d) {
+		if (d[1] == only_year_str[2]) {//compare year
+			if (d[4] == 'w' + (event_data.index + 1)) {
+				//console.log(d);
+				PidArray[countPid] = d[0];
+				WeightAray[countPid + 1] = parseFloat(d[3]);
+				countPid++;
+
+			}
+		}
+	})
+	array = WeightAray.slice(1, WeightAray.length).sort();
+	console.log(array);
+	var chart = c3.generate({
+		data : {
+			columns : [ WeightAray,
+
+			],
+			type : 'bar',
+			colors: {
+				'Weight-PID': '#FFA500',
+	        },
+			groups : [ [ 'Weight-PID' ] ]
+		},
+		bindto : '#'+id_specific_topic + repet,
+		grid : {
+			y : {
+				lines : [ {
+					value : 0
+				} ]
+			}
+		},
+		axis : {
+			x : {
+				label : {
+					text : 'Pids',
+					position : 'outer-middle',
+				},
+				type : 'category',
+				tick : {
+					rotate : 85,
+					multiline : false
+				},
+				categories : PidArray
+			},
+			tick : {
+				x : {
+
+					multiline : false,
+					culling : {
+						max : 1
+					},
+				},
+				label : {
+					text : 'PID',
+					position : 'center-bottom',
+
+				},
+			},
+			y : {
+				label : {
+					text : 'Nominal value (Million $)',
+					position : 'outer-middle',
+				},
+				max : array[array.length]+0.2,
+				min : 0,
+				padding : {
+					top : 0,
+					bottom : 0
+				}
+			}
+		}
+	});
+
+	/*console.log("id_specific_topic", id_specific_topic);
+	var idString=event_data.id+repet;//--------------------------------------------------
+	document.getElementById(idString).innerHTML = 'Associated topic  T'//-----------------
 			+ (event_data.index + 1);
 	var PidArray = [];
 	var WeightAray = [ 'Weight-PID' ];
@@ -344,7 +577,7 @@ function showPidWhenGivenTopicAndYear(event_data, data, id_specific_topic,repet)
 			},
 			groups : [ [ 'Weight-PID' ] ]
 		},
-		bindto : '#' + id_specific_topic + repet,
+		bindto : '#' + id_specific_topic + repet,//-------------------------
 		size : {
 			width : 480
 		// height: 300
@@ -402,7 +635,7 @@ function showPidWhenGivenTopicAndYear(event_data, data, id_specific_topic,repet)
 				}
 			}
 		}
-	});
+	});*/
 	var string = id_specific_topic + repet;
 	
 	var x = document.getElementById(string);
